d4390997c30c2eef5d880a8a50ef27a675b8d799
Nickolay <kolaz92@gmail.com>
2024-07-15 20:14:25 +0300

    strlt app

--- front/main.py
+++ front/main.py
@@ -2,25 +2,33 @@ import requests
 import streamlit as st
 import json
 
-def main():
+defIP, defPort = '84.201.152.230' , '8000'
 
-    st.title("Image classification")
+def get_log():
+    gres = requests.get(f"http://{defIP}:{defPort}/log")
+    st.text(f'Log:\n{gres.json()}')
+
+def main():
+    st.title("Сlassification of weather types")
 
     image = st.file_uploader("Choose an image", type=['jpg', 'jpeg'])
 
     if st.button("Classify!") and image is not None:
         st.image(image)
         files = {"file": image.getvalue()}
-        res = requests.post("http://127.0.0.1:8000/classify", files=files).json()
+        res = requests.post(f"http://{defIP}:{defPort}/classify", files=files).json()
         st.write(f'Class name: {res["class_name"]}, class index: {res["class_index"]}')
-        # st.write(json.loads(res.text)['prediction'])
+        get_log()
+
+    st.title("Test of toxicity level")
 
     txt = st.text_input('here')
 
     if st.button('send'):
         dat = {'text' : txt}
-        res = requests.post("http://127.0.0.1:8000/clf_text", json=dat)
+        res = requests.post(f"http://{defIP}:{defPort}/clf_text", json=dat)
         st.write(f"Probability of toxicity {res.json()['prob_of_tox']}")
+        get_log()
 
 if __name__ == '__main__':
     main()
